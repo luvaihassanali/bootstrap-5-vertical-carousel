f66bb486d88f61ef3c389e81de012564230b0485
Luvai Hassanali <luvaihassanali@gmail.com>
2022-02-11 23:28:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+});
+
+$('#vertical-carousel').bind('mousewheel DOMMouseScroll', function (e) {
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        $(this).carousel('prev');
+    }
+    else {
+        $(this).carousel('next');
+
+    }
+});
+
+$("#vertical-carousel").on("touchstart", function (event) {
+    var yTouchPointStart = event.originalEvent.touches[0].pageY;
+    $(this).on("touchmove", function (event) {
+        var yTouchPointEnd = event.originalEvent.touches[0].pageY;
+        if (Math.floor(yTouchPointStart - yTouchPointEnd) > 1) {
+            $(".carousel").carousel('next');
+        }
+        else if (Math.floor(yTouchPointStart - yTouchPointEnd) < -1) {
+            $(".carousel").carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
